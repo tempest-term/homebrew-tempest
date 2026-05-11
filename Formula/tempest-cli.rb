351f01a14480fa513bdf13c407bdfe2911fb60fc
Tempest CI <ci@gotempest.app>
2026-05-11 08:17:12 +0000

    tempest-cli 3.9.0

---
+++ Formula/tempest-cli.rb
@@ -0,0 +1,21 @@
+class TempestCli < Formula
+  desc "All-platform terminal emulator CLI: SSH, Telnet, RCON, FS, MCP server"
+  homepage "https://gotempest.app"
+  version "3.9.0"
+  license :cannot_represent
+
+  on_macos do
+    on_arm do
+      url "https://download.gotempest.app/cli/tempest-#{version}-darwin-arm64.tar.gz"
+      sha256 "62d90cbbb4cfac09c90f3f51795d22114a2b74400b8e9acb1f5c73be9498a673"
+    end
+  end
+
+  def install
+    bin.install "tempest"
+  end
+
+  test do
+    assert_match version.to_s, shell_output("#{bin}/tempest --version")
+  end
+end
