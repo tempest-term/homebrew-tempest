0fae36461862bc2b6a0613908076c52824084cee
Tempest CI <ci@gotempest.app>
2026-06-27 17:59:03 +0000

    tempest 3.12.0

--- Casks/tempest.rb
+++ Casks/tempest.rb
@@ -1,6 +1,6 @@
 cask "tempest" do
-  version "3.11.0"
-  sha256 "c8bb8ba207cb8a6dc12c5c0856886d0b862e6cba8066841f3bdca99100b5a5a4"
+  version "3.12.0"
+  sha256 "1a376fe6a4cbb92048a25db3332fb2f30bee205353d458b51757769272d6e691"
 
   url "https://download.gotempest.app/Tempest-#{version}-arm64-mac.zip"
   name "Tempest"
